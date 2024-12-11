29ef9ce1e47592ac0939a3bf686785afa8f43b7e
Graceli95 <graceli9095@gmail.com>
2024-12-11 07:41:44 -0700

    add files

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,24 @@
 package model;
 //Create an ArrayList to store names of students.
-public class ArrayList {
+public class Student {
     String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
--- src/utils/HandsOnArrayList.java
+++ src/utils/HandsOnArrayList.java
@@ -1,4 +1,36 @@
 package utils;
 
+import model.Student;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class HandsOnArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> studentNames = new ArrayList<>();
+//        Student student1 = new Student("Tom");
+//        Student student2 = new Student("Chris");
+//        Student student3 = new Student("Jerry");
+//        Student student4 = new Student("Steven");
+//        Student student5 = new Student("Jack");
+
+        studentNames.add("Tom");
+        studentNames.add("Jack");
+        studentNames.add("Chris");
+        studentNames.add("Steven");
+        studentNames.add("Jerry");
+
+        System.out.println(studentNames);
+
+        studentNames.remove("Jerry");
+        System.out.println("After removing a name, the updated list " + studentNames);
+        Collections.sort(studentNames);
+        System.out.println("after sort the list alphabetically");
+        System.out.println("print the sorted list " + studentNames);
+
+
+
+
+    }
+
 }
